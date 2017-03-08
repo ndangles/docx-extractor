6f51e3457d3fc5f65203130fc8dd7f332461e43b
Nick Dangles <ncdangl@ilstu.edu>
2017-03-08 12:35:42 -0600

    testing

--- index.js
+++ index.js
@@ -8,51 +8,52 @@ var parser = new xml2js.Parser()
 
 exports.extractComments = function(filename) {    
       
+      console.log(filename);
 
-	if(filename.indexOf(".docx")>-1){
-                var newFile = filename+'.zip';
-                fse.copy(__dirname+'/'+filename, __dirname+'/tmp/'+filename, function(err){
-                    fs.rename(__dirname+'/tmp/'+filename, __dirname+'/tmp/'+newFile, function(err) {
-                        fs.createReadStream(__dirname+'/tmp/'+newFile).pipe(unzip.Extract({ path: __dirname+'/tmp/'+filename})).on('close', function () {
-                            fs.readFile(__dirname + '/tmp/'+filename+'/word/comments.xml', function(err, data) {
-                                 if(err){
+	// if(filename.indexOf(".docx")>-1){
+ //                var newFile = filename+'.zip';
+ //                fse.copy(__dirname+'/'+filename, __dirname+'/tmp/'+filename, function(err){
+ //                    fs.rename(__dirname+'/tmp/'+filename, __dirname+'/tmp/'+newFile, function(err) {
+ //                        fs.createReadStream(__dirname+'/tmp/'+newFile).pipe(unzip.Extract({ path: __dirname+'/tmp/'+filename})).on('close', function () {
+ //                            fs.readFile(__dirname + '/tmp/'+filename+'/word/comments.xml', function(err, data) {
+ //                                 if(err){
                                     
-                                    return console.log("This document does not appear to have any comments");
+ //                                    return console.log("This document does not appear to have any comments");
                                 
-                                } else{
-                                    parser.parseString(data, function (err, result) {
+ //                                } else{
+ //                                    parser.parseString(data, function (err, result) {
 
-                                        parsedData = JSON.stringify(result);
-                                        var file = 'temp.json';
-                                        jsonfile.writeFile(file, parsedData, function(err){
-                                            jsonfile.readFile(file, function(err, obj) {
-                                                var jsonData = JSON.parse(obj);
-                                                var comments = [];
+ //                                        parsedData = JSON.stringify(result);
+ //                                        var file = 'temp.json';
+ //                                        jsonfile.writeFile(file, parsedData, function(err){
+ //                                            jsonfile.readFile(file, function(err, obj) {
+ //                                                var jsonData = JSON.parse(obj);
+ //                                                var comments = [];
 
-                                                for(i=0; i<2000; i++){
+ //                                                for(i=0; i<2000; i++){
 
-                                                try{
-                                                    var test = pointer.get(jsonData, '/w:comments/w:comment/'+i+'/w:p/0/w:r/1/w:t');
-                                                    var newComment = test[0].replace(/\W/g, ' ');
-                                                    comments[i] = newComment;
-                                                    console.log(comments[i]); 
-                                                }catch(e){
-                                                    return comments; //no more comments to parse, return 
+ //                                                try{
+ //                                                    var test = pointer.get(jsonData, '/w:comments/w:comment/'+i+'/w:p/0/w:r/1/w:t');
+ //                                                    var newComment = test[0].replace(/\W/g, ' ');
+ //                                                    comments[i] = newComment;
+ //                                                    console.log(comments[i]); 
+ //                                                }catch(e){
+ //                                                    return comments; //no more comments to parse, return 
                                                     
-                                                 }
-                                                }
-                                            });
-                                        });
+ //                                                 }
+ //                                                }
+ //                                            });
+ //                                        });
 
-                                    });
-                                }
-                            });
-                        });
-                    });
-                });
-          } else {
-          return console.log("The file you are passing into the function is not a 'docx' file");
-      }
+ //                                    });
+ //                                }
+ //                            });
+ //                        });
+ //                    });
+ //                });
+ //          } else {
+ //          return console.log("The file you are passing into the function is not a 'docx' file");
+ //      }
 
 
 
