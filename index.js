0f8557d5be38dbb183dc4cf04c0434c9f769f0d4
Nick Dangles <ncdangl@ilstu.edu>
2017-03-09 13:25:02 -0600

    minor changes

--- index.js
+++ index.js
@@ -467,7 +467,7 @@ exports.getHyperlinks = function(filepath, callback) {
 
                                                 try{
                                                     var test = pointer.get(jsonData, '/w:document/w:body/w:p/'+i+'/w:hyperlink/w:r/w:t');
-                                                    if(test!=null){
+                                                    if(test!=""){
                                                         hyperlinks[i] = test;
                                                     } 
                                                 }catch(e){
